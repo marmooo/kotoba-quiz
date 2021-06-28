043ef8820e8b0257c52b9a2150251ec57c8f3977
marmooo <marmooo@users.noreply.github.com>
2021-06-29 07:58:08 +0900

    cleanup

--- docs/src/conv.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const YomiDict = require('yomi-dict');
-const readEachLineSync = require('read-each-line-sync');
-
-readEachLineSync('4.lst', 'utf8', (line) => {
-  let [_, word2] = line.split('\t');
-  YomiDict.get(word2);
-});
