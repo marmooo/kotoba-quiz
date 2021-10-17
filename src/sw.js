9843104fa06031a7fea74f5c210a1c802982407a
marmooo <marmooo@users.noreply.github.com>
2021-10-17 13:48:05 +0900

    bump bootstrap from 5.1.2 to 5.1.3

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = '2021-10-15 20:33';
+var CACHE_NAME = '2021-10-17 13:42';
 var urlsToCache = [
   "/kotoba-quiz/",
   "/kotoba-quiz/hira.lst",
@@ -10,7 +10,7 @@ var urlsToCache = [
   "/kotoba-quiz/mp3/correct3.mp3",
   "/kotoba-quiz/favicon/original.svg",
   "/kotoba-quiz/signature_pad.umd.min.js",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs/dist/tf.min.js",
 ];
 
