6b8793a19ab4d3fe86571a5d2f15e788d8bd9026
marmooo <marmooo@users.noreply.github.com>
2022-03-08 00:22:36 +0900

    bump signature_pad from 4.0.1 to 4.0.2

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = '2022-01-30 12:35';
+var CACHE_NAME = '2022-03-08 00:20';
 var urlsToCache = [
   "/kotoba-quiz/",
   "/kotoba-quiz/hira.lst",
@@ -10,8 +10,7 @@ var urlsToCache = [
   "/kotoba-quiz/mp3/end.mp3",
   "/kotoba-quiz/mp3/correct3.mp3",
   "/kotoba-quiz/favicon/original.svg",
-  "/kotoba-quiz/signature_pad.umd.min.js",
-  "https://cdn.jsdelivr.net/npm/signature_pad@4.0.1/dist/signature_pad.umd.min.js",
+  "https://cdn.jsdelivr.net/npm/signature_pad@4.0.2/dist/signature_pad.umd.min.js",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@3.9.0/dist/tf.min.js",
 ];
