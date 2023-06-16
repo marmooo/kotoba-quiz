9a80eca6c124efdc42a33dcd0be59b218883b4b6
marmooo <marmooo@users.noreply.github.com>
2023-06-17 00:59:38 +0900

    update

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "2023-05-21 14:10";
+var CACHE_NAME = "2023-06-17 01:00";
 var urlsToCache = [
   "/kotoba-quiz/",
   "/kotoba-quiz/hira.lst",
@@ -10,8 +10,8 @@ var urlsToCache = [
   "/kotoba-quiz/model/group1-shard1of1.bin",
   "/kotoba-quiz/mp3/end.mp3",
   "/kotoba-quiz/mp3/correct3.mp3",
+  "/kotoba-quiz/img/surfing-js.png",
   "/kotoba-quiz/favicon/favicon.svg",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/signature_pad@4.1.5/dist/signature_pad.umd.min.js",
   "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.6.0/dist/tf.min.js",
   "https://cdn.jsdelivr.net/npm/animate.css@4.1.1/animate.min.css",
