4fddee646e7d6201cb5d71e99f685a32d9c30126
marmooo <marmooo@users.noreply.github.com>
2024-01-02 16:21:19 +0900

    bump tfjs from 4.13.0 to 4.15.0

--- src/sw.js
+++ src/sw.js
@@ -12,7 +12,7 @@ const urlsToCache = [
   "/kotoba-quiz/img/surfing-js.png",
   "/kotoba-quiz/favicon/favicon.svg",
   "https://cdn.jsdelivr.net/npm/signature_pad@4.1.7/dist/signature_pad.umd.min.js",
-  "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.13.0/dist/tf.min.js",
+  "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.15.0/dist/tf.min.js",
   "https://cdn.jsdelivr.net/npm/animate.css@4.1.1/animate.min.css",
 ];
 
